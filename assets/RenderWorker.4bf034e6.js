99525f8568641c7716affefd76a1e5d70837f652
robertlong <robert@robertlong.me>
2022-04-08 20:36:51 +0000

    Deploying to gh-pages from @ matrix-org/thirdroom@02036b243194bfd9882516e42d1dd87d324725e1 🚀

---
+++ assets/RenderWorker.4bf034e6.js
@@ -0,0 +1,1 @@
+function e(){return new Worker("/assets/RenderWorker.ab502a6c.js",{type:"module"})}export{e as default};
--- assets/RenderWorker.ec4a2fbd.js
+++
@@ -1,1 +0,0 @@
-function e(){return new Worker("/assets/RenderWorker.650b119f.js",{type:"module"})}export{e as default};
